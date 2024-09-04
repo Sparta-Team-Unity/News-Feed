2ecc42685c297fd1319c75b50cdeeb84eeb42a6d
kang-sumin <min001117@gmail.com>
2024-09-04 17:52:08 +0900

    [UPDATE] User 객체랑 양방향 연관관계 생성

--- src/main/java/com/sparta/newsfeed/domain/repository/PostRepository.java
+++ src/main/java/com/sparta/newsfeed/domain/repository/PostRepository.java
@@ -1,10 +1,13 @@
 package com.sparta.newsfeed.domain.repository;
 
 import com.sparta.newsfeed.domain.entity.Post;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Integer> {
     Optional<Post> findById(Integer postId);
+
+
 }
