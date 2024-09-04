5a2378e9a8cc5230da8221435d036f3109ec3ad1
kang-sumin <min001117@gmail.com>
2024-09-04 14:32:45 +0900

    [UPDATE] postRepository

--- src/main/java/com/sparta/newsfeed/domain/repository/PostRepository.java
+++ src/main/java/com/sparta/newsfeed/domain/repository/PostRepository.java
@@ -3,5 +3,5 @@ package com.sparta.newsfeed.domain.repository;
 import com.sparta.newsfeed.domain.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Integer> {
 }
