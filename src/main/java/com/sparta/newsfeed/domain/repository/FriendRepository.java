73039c75764eb8c1ad518d6742b1eb76af6d51d6
Ackrilll <skawlsgus2@naver.com>
2024-09-04 15:13:24 +0900

    [Update] Friend entity, FriendRepository 업데이트

--- src/main/java/com/sparta/newsfeed/domain/repository/FriendRepository.java
+++ src/main/java/com/sparta/newsfeed/domain/repository/FriendRepository.java
@@ -1,8 +1,13 @@
 package com.sparta.newsfeed.domain.repository;
 
 import com.sparta.newsfeed.domain.entity.Friend;
+import com.sparta.newsfeed.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
 
-public interface FriendRepository extends JpaRepository<Friend, Long> {
+
+public interface FriendRepository extends JpaRepository<Friend, Integer> {
+    Optional<Friend> findByFriendId(Integer friendId);
 }
