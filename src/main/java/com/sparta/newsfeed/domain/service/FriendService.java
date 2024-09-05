f225b7b34cc89e728c6d1ae2edec9f4f3b15cdc8
Ackrilll <skawlsgus2@naver.com>
2024-09-05 18:51:51 +0900

    [Refactory] FriendService 리팩토링

--- src/main/java/com/sparta/newsfeed/domain/service/FriendService.java
+++ src/main/java/com/sparta/newsfeed/domain/service/FriendService.java
@@ -25,42 +25,43 @@ public class FriendService {
     private final FriendRepository friendRepository;
     private final UserRepository userRepository;
 
+    //친구 신청 메서드
     public void addFriend(Integer followid, UserDto userDto) {
-        User toUser = userRepository.findById(followid).orElseThrow(()->new NoSuchElementException("User Not Found"));
+        User toUser = targetUser(followid);
         User currentUser = currentUser(userDto);
-        if(toUser.equals(currentUser)) {
+        if(isSamePerson(toUser, currentUser)) {
+            //친구 신청 보낸 사람이랑 받은사람 같을때
             throw new SamePersonException();
         }
         else {
-            if(friendRepository.existsByToUserAndFromUser(toUser, currentUser)
-                    || friendRepository.existsByToUserAndFromUser(currentUser, toUser)){
+            if(isExistFriendsRequest(toUser, currentUser)) {
+                // 친구 신청 보낸 사람이랑 받은사람 다르고 신청 내용 이미 존재할 때
                 throw new DuplicateFriendException();
             }
             friendRepository.save(new Friend(toUser, currentUser));
         }
     }
 
+    // 친구목록 조회 메서드
     @Transactional(readOnly = true)
     public FriendResponseDto friendsInquiry(UserDto userDto) {
 
         // user -> 로그인 중인 현재 사용자
         User currentUser = currentUser(userDto);
         //friendList는 친구 신청을 하거나 받은 사람중 수락된 Friend 객체들만 전부 모은 리스트
-        List<Friend> friendList = friendRepository.findByFromUserAndIsAccepted(currentUser,true);
-        friendList.addAll(friendRepository.findByToUserAndIsAccepted(currentUser,true));
+        List<Friend> friendList = findFriends(currentUser);
+
 
         List<FollowDto> responseList = new ArrayList<>();
         // friendList 순회 -> FollowDto 객체들을 만들어 responseList 갱신
         for (Friend friend : friendList) {
-            int friendId = friend.getFromUser().getUserId() == currentUser.getUserId()
-                    ? friend.getToUser().getUserId()
-                    : friend.getFromUser().getUserId();
-
+            int friendId = findFriendId(friend, currentUser);
             responseList.add(new FollowDto(friendId));
         }
         return new FriendResponseDto(responseList);
     }
 
+    //친구 요청 수락 대기 목록 조회(로그인한 사용자에게 온 친구 요청)
     @Transactional(readOnly = true)
     public WaitsResponseDto waitsInquiry(UserDto userDto) {
 
@@ -68,8 +69,7 @@ public class FriendService {
         // user -> 로그인 중인 현재 사용자
         User currentUser = currentUser(userDto);
         // 수락 대기중인 Friend 객체 담은 wiatList 리스트 순회 waitsDto 담은 responseList 리스트 갱신
-        List<Friend> wiatList = friendRepository.findByToUserAndIsAccepted(currentUser,false);
-
+        List<Friend> wiatList = findFriendWaits(currentUser);
         List<WaitsDto> responseList = new ArrayList<>();
 
         for (Friend wait : wiatList) {
@@ -86,14 +86,14 @@ public class FriendService {
         // user -> 로그인 중인 현재 사용자
         User currentUser = currentUser(userDto);
         //fromUser -> 친구 요청을 보낸 유저
-        User fromUser = userRepository.findById(followid).orElseThrow(()->new NoSuchElementException("User Not Found"));
+        User fromUser = targetUser(followid);
 
         // fromUser가 user에게 보낸 친구 요청 있는지 확인
         // 이미 친구 관계인지 확인
-        if(friendRepository.existsByToUserAndFromUser(currentUser, fromUser)){
-            if(friendRepository.existsByToUserAndFromUserAndIsAccepted(currentUser, fromUser, false)){
+        if(isExistFriendsRequest(fromUser, currentUser)) {
+            if(IsWaitRequest(currentUser, fromUser)){
                 // 수락 대기중인 친구 요청 찾았을 때 -> 수락 상태로 변화
-                friendRepository.updateFriendRequestStatus(fromUser,currentUser,true);
+                reciveFriendRequest(fromUser, currentUser);
             }
             else {
                 //이미 친구가 된 요청 찾았을 때
@@ -110,27 +110,80 @@ public class FriendService {
         // fromUser 추후에 JWT 에서 추출할 것
         // user -> 로그인 중인 현재 사용자
         User currentUser = currentUser(userDto);
-        User targetUser = userRepository.findById(followid).orElseThrow(()->new NoSuchElementException("User Not Found"));
+        User targetUser = targetUser(followid);
 
         //친구 관계인지 확인
         //메서드 따로 빼기
 
-        if(friendRepository.existsByToUserAndFromUserAndIsAccepted(currentUser, targetUser, true)) {
-
+        if(IsAlreadyFriend(currentUser, targetUser)) {
             //친구 일 때
-            Friend friend = friendRepository.findByToUserAndFromUserAndIsAccepted(currentUser,targetUser,true);
+            Friend friend = findFriend(currentUser, targetUser);
             friendRepository.delete(friend);
-        } else if (friendRepository.existsByToUserAndFromUserAndIsAccepted(targetUser,currentUser, true)) {
+        } else if (IsAlreadyFriend(targetUser, currentUser)) {
             // 친구 일 때
-            Friend friend = friendRepository.findByToUserAndFromUserAndIsAccepted(targetUser,currentUser,true);
+            Friend friend = findFriend(targetUser, currentUser);
             friendRepository.delete(friend);
         } else {
             // 친구가 아닐 때
             throw new NoFriend();
         }
     }
+    //------------------------------------ 유틸 메서드 ----------------------------------------------//
 
     public User currentUser(UserDto userDto){
         return userRepository.findById(userDto.getId()).orElseThrow(()->new NoSuchElementException("User Not Found"));
+    }
+
+    public User targetUser(Integer followid) {
+        return userRepository.findById(followid).orElseThrow(()->new NoSuchElementException("User Not Found"));
+    }
+    public boolean isSamePerson(User user1, User user2){
+        if(user1.equals(user2)){
+            return true;
+        }
+        return false;
+    }
+    public boolean isExistFriendsRequest(User user1, User user2){
+        if(friendRepository.existsByToUserAndFromUser(user1, user2)
+                || friendRepository.existsByToUserAndFromUser(user2, user1)){
+            return true;
+        }
+        return false;
+    }
+    public List<Friend> findFriends(User user) {
+        List<Friend> friendList = friendRepository.findByFromUserAndIsAccepted(user,true);
+        friendList.addAll(friendRepository.findByToUserAndIsAccepted(user,true));
+        return friendList;
+    }
+
+    public int findFriendId(Friend friend, User user) {
+        if(friend.getFromUser().getUserId() == user.getUserId()){
+            return friend.getToUser().getUserId();
+        }
+        else {
+            return friend.getFromUser().getUserId();
+        }
+    }
+    public List<Friend> findFriendWaits(User user) {
+       return friendRepository.findByToUserAndIsAccepted(user,false);
+    }
+    public boolean IsWaitRequest(User user1, User user2) {
+        if(friendRepository.existsByToUserAndFromUserAndIsAccepted(user1, user2, false)){
+            return true;
+        }
+        return false;
+    }
+    public void reciveFriendRequest(User user1, User user2) {
+        friendRepository.updateFriendRequestStatus(user1,user2,true);
+    }
+    public boolean IsAlreadyFriend(User user1, User user2) {
+        if(friendRepository.existsByToUserAndFromUserAndIsAccepted(user1, user2, true)){
+            return true;
+        }
+        return false;
+    }
+    public Friend findFriend(User user1, User user2) {
+        Friend friend = friendRepository.findByToUserAndFromUserAndIsAccepted(user1,user2,true);
+        return friend;
     }
 }
