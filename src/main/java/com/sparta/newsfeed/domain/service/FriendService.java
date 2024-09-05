ff83f4ffa8b1b9997febba1aea3dacb314bdae5c
Ackrilll <skawlsgus2@naver.com>
2024-09-05 17:29:02 +0900

    [Refactory] FriendService 리팩토링

--- src/main/java/com/sparta/newsfeed/domain/service/FriendService.java
+++ src/main/java/com/sparta/newsfeed/domain/service/FriendService.java
@@ -27,16 +27,16 @@ public class FriendService {
 
     public void addFriend(Integer followid, UserDto userDto) {
         User toUser = userRepository.findById(followid).orElseThrow(()->new NoSuchElementException("User Not Found"));
-        User fromUser = userRepository.findById(userDto.getId()).orElseThrow(()->new NoSuchElementException("User Not Found"));
-        if(toUser.equals(fromUser)) {
+        User currentUser = currentUser(userDto);
+        if(toUser.equals(currentUser)) {
             throw new SamePersonException();
         }
         else {
-            if(friendRepository.existsByToUserAndFromUser(toUser, fromUser)
-                    || friendRepository.existsByToUserAndFromUser(fromUser, toUser)){
+            if(friendRepository.existsByToUserAndFromUser(toUser, currentUser)
+                    || friendRepository.existsByToUserAndFromUser(currentUser, toUser)){
                 throw new DuplicateFriendException();
             }
-            friendRepository.save(new Friend(toUser, fromUser));
+            friendRepository.save(new Friend(toUser, currentUser));
         }
     }
 
@@ -44,16 +44,15 @@ public class FriendService {
     public FriendResponseDto friendsInquiry(UserDto userDto) {
 
         // user -> 로그인 중인 현재 사용자
-        User user = userRepository.findById(userDto.getId()).orElseThrow(()->new NoSuchElementException("User Not Found"));
-
+        User currentUser = currentUser(userDto);
         //friendList는 친구 신청을 하거나 받은 사람중 수락된 Friend 객체들만 전부 모은 리스트
-        List<Friend> friendList = friendRepository.findByFromUserAndIsAccepted(user,true);
-        friendList.addAll(friendRepository.findByToUserAndIsAccepted(user,true));
+        List<Friend> friendList = friendRepository.findByFromUserAndIsAccepted(currentUser,true);
+        friendList.addAll(friendRepository.findByToUserAndIsAccepted(currentUser,true));
 
         List<FollowDto> responseList = new ArrayList<>();
         // friendList 순회 -> FollowDto 객체들을 만들어 responseList 갱신
         for (Friend friend : friendList) {
-            int friendId = friend.getFromUser().getUserId() == user.getUserId()
+            int friendId = friend.getFromUser().getUserId() == currentUser.getUserId()
                     ? friend.getToUser().getUserId()
                     : friend.getFromUser().getUserId();
 
@@ -67,10 +66,9 @@ public class FriendService {
 
 
         // user -> 로그인 중인 현재 사용자
-        User user = userRepository.findById(userDto.getId()).orElseThrow(()->new NoSuchElementException("User Not Found"));
-
+        User currentUser = currentUser(userDto);
         // 수락 대기중인 Friend 객체 담은 wiatList 리스트 순회 waitsDto 담은 responseList 리스트 갱신
-        List<Friend> wiatList = friendRepository.findByToUserAndIsAccepted(user,false);
+        List<Friend> wiatList = friendRepository.findByToUserAndIsAccepted(currentUser,false);
 
         List<WaitsDto> responseList = new ArrayList<>();
 
@@ -86,17 +84,16 @@ public class FriendService {
 
         // fromUser 추후에 JWT 에서 추출할 것
         // user -> 로그인 중인 현재 사용자
-        User user = userRepository.findById(userDto.getId()).orElseThrow(()->new NoSuchElementException("User Not Found"));
-
+        User currentUser = currentUser(userDto);
         //fromUser -> 친구 요청을 보낸 유저
         User fromUser = userRepository.findById(followid).orElseThrow(()->new NoSuchElementException("User Not Found"));
 
         // fromUser가 user에게 보낸 친구 요청 있는지 확인
         // 이미 친구 관계인지 확인
-        if(friendRepository.existsByToUserAndFromUser(user, fromUser)){
-            if(friendRepository.existsByToUserAndFromUserAndIsAccepted(user, fromUser, false)){
+        if(friendRepository.existsByToUserAndFromUser(currentUser, fromUser)){
+            if(friendRepository.existsByToUserAndFromUserAndIsAccepted(currentUser, fromUser, false)){
                 // 수락 대기중인 친구 요청 찾았을 때 -> 수락 상태로 변화
-                friendRepository.updateFriendRequestStatus(fromUser,user,true);
+                friendRepository.updateFriendRequestStatus(fromUser,currentUser,true);
             }
             else {
                 //이미 친구가 된 요청 찾았을 때
@@ -112,25 +109,28 @@ public class FriendService {
     public void deleteFriend(Integer followid, UserDto userDto) {
         // fromUser 추후에 JWT 에서 추출할 것
         // user -> 로그인 중인 현재 사용자
-        User user = userRepository.findById(userDto.getId()).orElseThrow(()->new NoSuchElementException("User Not Found"));
-
+        User currentUser = currentUser(userDto);
         User targetUser = userRepository.findById(followid).orElseThrow(()->new NoSuchElementException("User Not Found"));
 
         //친구 관계인지 확인
         //메서드 따로 빼기
 
-        if(friendRepository.existsByToUserAndFromUserAndIsAccepted(user, targetUser, true)) {
+        if(friendRepository.existsByToUserAndFromUserAndIsAccepted(currentUser, targetUser, true)) {
 
             //친구 일 때
-            Friend friend = friendRepository.findByToUserAndFromUserAndIsAccepted(user,targetUser,true);
+            Friend friend = friendRepository.findByToUserAndFromUserAndIsAccepted(currentUser,targetUser,true);
             friendRepository.delete(friend);
-        } else if (friendRepository.existsByToUserAndFromUserAndIsAccepted(targetUser,user, true)) {
+        } else if (friendRepository.existsByToUserAndFromUserAndIsAccepted(targetUser,currentUser, true)) {
             // 친구 일 때
-            Friend friend = friendRepository.findByToUserAndFromUserAndIsAccepted(targetUser,user,true);
+            Friend friend = friendRepository.findByToUserAndFromUserAndIsAccepted(targetUser,currentUser,true);
             friendRepository.delete(friend);
         } else {
             // 친구가 아닐 때
             throw new NoFriend();
         }
+    }
+
+    public User currentUser(UserDto userDto){
+        return userRepository.findById(userDto.getId()).orElseThrow(()->new NoSuchElementException("User Not Found"));
     }
 }
