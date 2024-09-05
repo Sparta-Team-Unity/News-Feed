c11161195fddd4fdab4b56946cf78ed65f4d9310
Ackrilll <skawlsgus2@naver.com>
2024-09-05 20:09:45 +0900

    [REFACTOR] 안쓰는 Exception 제거

--- src/main/java/com/sparta/newsfeed/domain/exception/SamePersonException.java
+++
@@ -1,7 +0,0 @@
-package com.sparta.newsfeed.domain.exception;
-
-public class SamePersonException extends RuntimeException {
-    public SamePersonException() {
-        super("자신에게는 친구 신청할 수 없습니다.");
-    }
-}
